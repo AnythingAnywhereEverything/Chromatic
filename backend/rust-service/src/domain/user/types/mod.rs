6dfed6c04ef6bea83c971ab005c0d43cc1fefdc6
Zartex <56326190+ZartexVertagen@users.noreply.github.com>
2026-06-20 21:09:02 +0700

    chore(domain): add password to module

--- backend/rust-service/src/domain/user/types/mod.rs
+++ backend/rust-service/src/domain/user/types/mod.rs
@@ -1,7 +1,9 @@
 pub mod username;
 pub mod email;
 pub mod display_name;
+pub mod password;
 
 pub use display_name::DisplayName;
 pub use username::Username;
 pub use email::Email;
+pub use password::Password;
