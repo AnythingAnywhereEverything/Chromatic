b1000a0b939bea23523c3289d1cdb8888fa0fb1a
Zartex <56326190+ZartexVertagen@users.noreply.github.com>
2026-06-25 04:05:42 +0700

    chore: remove oauth route

--- backend/rust-service/src/api/routes/auth_routes.rs
+++ backend/rust-service/src/api/routes/auth_routes.rs
@@ -2,7 +2,7 @@ use axum::{Router, routing::post};
 
 use crate::{
     api::handlers::auth_handlers::{
-        delete_handler, login_handler, logout_handler, oauth_handler, register_handler
+        delete_handler, login_handler, logout_handler, register_handler
     },
     application::state::SharedState,
 };
@@ -12,7 +12,6 @@ pub fn routes() -> Router<SharedState> {
         .route("/login", post(login_handler))
         .route("/logout", post(logout_handler))
         .route("/register", post(register_handler))
-        .route("/oauth", post(oauth_handler))
         
         .route("/delete", post(delete_handler))
 }
