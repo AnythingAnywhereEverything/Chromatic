174833b92aec2b1595d984d4a098472b469005be
Zartex <56326190+ZartexVertagen@users.noreply.github.com>
2026-07-01 08:08:21 +0700

    refactor(image): improve image cropping logic and enhance logging

--- backend/rust-service/src/application/service/media/processor/image.rs
+++ backend/rust-service/src/application/service/media/processor/image.rs
@@ -1,7 +1,6 @@
 use libvips::{VipsImage, ops};
 
-use crate::application::service::{errors::MediaServiceError};
-
+use crate::application::service::errors::MediaServiceError;
 
 pub fn resize_image(
     mut image: VipsImage,
@@ -11,8 +10,7 @@ pub fn resize_image(
     rz_height: u32,
 ) -> Result<VipsImage, MediaServiceError> {
     if width > rz_width || height > rz_height {
-        let scale = (rz_width as f64 / width as f64)
-            .min(rz_height as f64 / height as f64);
+        let scale = (rz_width as f64 / width as f64).min(rz_height as f64 / height as f64);
 
         image = ops::resize(&image, scale)?;
     }
@@ -27,29 +25,46 @@ pub fn crop_image_absolute(
     cr_height: u32,
     position: Option<(f32, f32)>,
 ) -> Result<VipsImage, MediaServiceError> {
-    // Calculate the top-left corner of the crop area based on the position parameter
-    // patch out of bounds check
+    let max_left = (width - cr_width) as i32;
+    let max_top = (height - cr_height) as i32;
+
     let (left, top) = match position {
         Some((x, y)) => {
-            let left = ((width as f32 - cr_width as f32) * x).round() as i32;
-            let top = ((height as f32 - cr_height as f32) * y).round() as i32;
-            (left.max(0).min((width - cr_width) as i32), top.max(0).min((height - cr_height) as i32))
+            let left = (x * width as f32).round() as i32;
+            let top = (y * height as f32).round() as i32;
+
+            (left.clamp(0, max_left), top.clamp(0, max_top))
         }
-        None => {
-            let left = ((width as f32 - cr_width as f32) / 2.0).round() as i32;
-            let top = ((height as f32 - cr_height as f32) / 2.0).round() as i32;
-            (left.max(0).min((width - cr_width) as i32), top.max(0).min((height - cr_height) as i32))
-        }
+        None => (max_left / 2, max_top / 2),
     };
 
     tracing::warn!(
-        "Cropping with absolute dimensions: {}x{}, position: ({}, {}), resulting dimensions: {}x{}",
-        cr_width, cr_height, left, top, cr_width, cr_height
+        "Cropping with\n\t absolute dimensions: {}x{},\n\t position: ({}, {}),\n\t resulting dimensions: {}x{}",
+        cr_width,
+        cr_height,
+        left,
+        top,
+        cr_width,
+        cr_height
     );
 
+    tracing::warn!(
+        "Cropping with\n\t original dimensions: {}x{},\n\t position: ({}, {}),\n\t resulting dimensions: {}x{}",
+        width,
+        height,
+        left,
+        top,
+        cr_width,
+        cr_height
+    );
 
-    
-    image = ops::extract_area(&image, left as i32, top as i32, cr_width as i32, cr_height as i32)?;
+    image = ops::extract_area(
+        &image,
+        left as i32,
+        top as i32,
+        cr_width as i32,
+        cr_height as i32,
+    )?;
 
     Ok(image)
 }
@@ -82,30 +97,27 @@ pub fn crop_image_ratio(
         return Err(MediaServiceError::InvalidScale);
     }
 
-    let (cr_width, cr_height) = match scale {
-        scale => {
-            let target_width = (width as f32 * scale).round() as u32;
-            let target_height = (height as f32 * scale).round() as u32;
+    let aspect = ratio.0 as f32 / ratio.1 as f32;
 
-            let ratio_width = ratio.0;
-            let ratio_height = ratio.1;
-
-            // Calculate the maximum width and height that maintains the aspect ratio
-            let max_width = (target_height as f32 * (ratio_width as f32 / ratio_height as f32)).round() as u32;
-            let max_height = (target_width as f32 * (ratio_height as f32 / ratio_width as f32)).round() as u32;
-
-            // Determine the final crop dimensions based on the target size and aspect ratio
-            if max_width <= target_width {
-                (max_width, target_height)
-            } else {
-                (target_width, max_height)
-            }
-        }
+    let (max_crop_width, max_crop_height) = if width as f32 / height as f32 >= aspect {
+        let h = height as f32;
+        let w = h * aspect;
+        (w.round() as u32, h.round() as u32)
+    } else {
+        let w = width as f32;
+        let h = w / aspect;
+        (w.round() as u32, h.round() as u32)
     };
+
+    let cr_width = (max_crop_width as f32 * scale).round() as u32;
+    let cr_height = (max_crop_height as f32 * scale).round() as u32;
 
     tracing::warn!(
         "Cropping with ratio: {:?}, scale: {}, resulting dimensions: {}x{}",
-        ratio, scale, cr_width, cr_height
+        ratio,
+        scale,
+        cr_width,
+        cr_height
     );
 
     crop_image_absolute(image, width, height, cr_width, cr_height, position)
