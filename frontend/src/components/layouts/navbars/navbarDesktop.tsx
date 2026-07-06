303d0837c093bdc5936147367c5b3f56beff1cbe
seinaru <82324764+K-Hongkaew@users.noreply.github.com>
2026-07-06 10:53:09 +0700

    feat: navbar (WIP)
    // * I'll change to for-loop later

--- frontend/src/components/layouts/navbars/navbarDesktop.tsx
+++ frontend/src/components/layouts/navbars/navbarDesktop.tsx
@@ -4,8 +4,14 @@ import { Field, Label } from '@components/ui/chormaticUI';
 import { IoSettingsOutline } from "react-icons/io5";
 import { IoHomeOutline } from "react-icons/io5";
 import { IoSearchOutline } from "react-icons/io5";
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { IoBookmarkOutline } from "react-icons/io5";
+import { IoChatbubbleOutline } from "react-icons/io5";
+import { HiOutlinePlus } from "react-icons/hi";
+
 const NavDesktop: React.FC = () => {
 
+    // * create array for nav then for-loop
     return (
     <nav className={style.navbar}>
         <div className={style.navTop}>
@@ -14,23 +20,51 @@ const NavDesktop: React.FC = () => {
             </Link>
         </div>
         <Field className={style.navMid}>
-            <ul>
-                <li><IoHomeOutline/> <Link className={style.hidden} href={'#'}>Home</Link></li>
-                <li><IoSearchOutline/><Link className={style.hidden} href={'#'}>Search</Link></li>
-                <li><Link className={style.hidden} href={'#'}>Notification</Link></li>
-                <li><Link className={style.hidden} href={'#'}>Bookmark</Link></li>
-                <li><Link className={style.hidden} href={'#'}>Chat</Link></li>
-                <li><Link className={style.hidden} href={'#'}>Post</Link></li>
+            <ul style={{width: "100%"}}>
+                <li>
+                    <Link className={style.flex} href={'#'}>
+                        <IoHomeOutline/><p className={style.hidden}>Home</p>
+                    </Link>
+                </li>
+                
+                <li>
+                    <Link className={style.flex} href={'#'}> 
+                        <IoSearchOutline/><p className={style.hidden}>Search</p>
+                    </Link>
+                </li>
+                
+                <li>
+                    <Link className={style.flex} href={'#'}> 
+                        <IoIosNotificationsOutline/><p className={style.hidden}>Notification</p>
+                    </Link>
+                </li>
+                
+                <li>
+                    <Link className={style.flex} href={'#'}> 
+                        <IoBookmarkOutline/><p className={style.hidden}>Bookmark</p>
+                    </Link>
+                </li>
+                
+                <li>
+                    <Link className={style.flex} href={'#'}> 
+                        <IoChatbubbleOutline/><p className={style.hidden}>Messages</p>
+                    </Link>
+                </li>
+
+                <li>
+                    <Link className={`${style.flex} ${style.post}`} href={'#'}> 
+                        <HiOutlinePlus/><p className={style.hidden}>Post</p>
+                    </Link>
+                </li>
             </ul>
         </Field>
+        
         <Field className={style.navBottom}>
             <Field orientation={'horizontal'}>
                 <div style={{display: 'flex', gap: '4px'}}>
-                    <img src="https://placehold.co/60" alt="" />
+                    <img src="https://placehold.co/40" alt="" />
                     <Label className={style.hidden}>Username</Label>
                 </div>
-
-                <IoSettingsOutline/>
             </Field>
         </Field>
     </nav>
