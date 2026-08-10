36c9b8d9ea8ced1df34939174c04bf3be4516934
seinaru <82324764+K-Hongkaew@users.noreply.github.com>
2026-08-11 01:45:42 +0700

    feat: testing component

--- frontend/src/app/(main)/test/page.tsx
+++ frontend/src/app/(main)/test/page.tsx
@@ -2,10 +2,12 @@
 
 import { Portal } from "@/app/_components/portal";
 import { AlertDialogue} from "@/app/_components/ui/chromatic/confirmation";
+import { CreateCommunityBtn } from "@/app/_components/ui/chromatic/createCom";
 import { CreatePost } from "@/app/_components/ui/chromatic/createPost";
 import { PostStatus, PostVisibility } from "@/app/_components/ui/chromatic/createPost/status";
 import {  Dialog, DialogClose, DialogContent, DialogDescription, DialogHeading,DialogTrigger } from "@/app/_components/ui/chromatic/dialogue";
 import { Dropdown, DropdownContent, DropdownItem, DropdownTrigger } from "@/app/_components/ui/chromatic/dropdown";
+import { Post } from "@/app/_components/ui/chromatic/post";
 import {
     Tooltip,
     TooltipAnchor,
@@ -210,7 +212,24 @@ const TestPage = () => {
                 onChange={(value) => setTestStatus(value)}
                 />
             </section>
-              
+            <section>
+              <Post 
+                id={""}
+                ownerId={""}
+                ownerName={""}
+                content={`
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at consequat sem. Donec tincidunt auctor nisl iaculis ultrices. Maecenas a velit purus. Nulla sit amet sem nulla. Morbi tincidunt purus sit amet eros bibendum, quis lobortis eros molestie. In accumsan bibendum velit non dapibus. Cras id varius lacus. Donec faucibus eget lorem mollis rutrum. Donec ante quam, pretium eget felis vitae, pulvinar efficitur nisi. Phasellus eget risus sollicitudin, posuere enim nec, placerat eros. Praesent maximus bibendum velit ut pretium. Praesent arcu turpis, euismod eu ex porta, scelerisque iaculis est. Nulla semper felis tortor, ut mattis elit viverra id. Duis vel hendrerit sem.
+                    Nullam posuere lectus nec lectus tempor dignissim. Nulla magna tortor, facilisis ac nisl vel, rutrum congue neque. Fusce a justo porta, venenatis quam et, euismod lorem. Duis sit amet maximus nunc. Quisque sed mattis mauris, non gravida metus. Ut faucibus erat lectus, eget tempus ex rutrum sed. Ut mollis ante nisl, et egestas risus sodales consectetur. Vivamus et arcu scelerisque, semper lorem et, feugiat tellus. Morbi sagittis eros sed iaculis consequat.
+                    Etiam sit amet pulvinar lectus. Aenean consectetur libero sollicitudin feugiat consequat. Ut ante lorem, dignissim ut massa sit amet, lacinia varius est. Pellentesque id est vitae magna sagittis tincidunt vel in nibh. Suspendisse ac congue sem. Praesent at mauris turpis. Morbi eleifend facilisis metus.
+                `}
+                like={0}
+                comment={[]} 
+                bookmark={false}/>
+            </section>
+
+            <section>
+                <CreateCommunityBtn/>
+            </section>
         </section>
     );
 };
