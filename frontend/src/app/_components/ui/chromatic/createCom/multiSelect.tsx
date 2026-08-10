46255b280bbaab4d59b0b593350b98e67f23f292
seinaru <82324764+K-Hongkaew@users.noreply.github.com>
2026-08-11 01:46:11 +0700

    feat: image cropper
    - multiselect

---
+++ frontend/src/app/_components/ui/chromatic/createCom/multiSelect.tsx
@@ -0,0 +1,25 @@
+
+// * For real
+export enum AcademicSubject {
+    ComputerScience = "Computer Science",
+   
+}
+
+interface MultiProps{
+    name: string
+    value: string
+}
+
+const MultiInterestTag:React.FC = () => {
+    return (
+        <section>
+            <select 
+            name="" 
+            id=""
+            multiple={true}
+            >
+            
+            </select>
+        </section>
+    )
+}
