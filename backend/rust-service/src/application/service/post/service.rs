fdfe05fa530c3b0413a0f1e11c00b182a32d2fef
seinaru <82324764+K-Hongkaew@users.noreply.github.com>
2026-08-10 20:47:36 +0700

    feat(post): WIP post service and mod

---
+++ backend/rust-service/src/application/service/post/service.rs
@@ -0,0 +1,31 @@
+// use crate::application::{service::errors::PostServiceError, state::AppState};
+
+pub struct PostService ;
+
+impl PostService {
+
+//     // todo: impl the image attachment
+//     pub async fn create_post(
+//         state: &AppState,
+//         user_id: i64,
+//         content: &str,
+//         status:  &str,
+//         // ? image: &str  
+//     ) -> Result<i64, PostServiceError> {
+//         let mut tx = state.db_pool.begin().await?;
+        
+//         let post_id = state.snowflake_generator.generate_id()?;
+
+//         Ok(post_id)
+//     }
+
+    // pub async fn update_post(
+    //     state: &AppState,
+    //     id: i64,
+    //     user_id: i64,
+    //     content: &str
+    // ) -> Result<(), PostServiceError> {
+    //     let mut tx = state.db_pool.begin().await?;
+
+    // }
+}
