33cc8f650578f15c520a298c9b4c13279945053f
seinaru <82324764+K-Hongkaew@users.noreply.github.com>
2026-08-13 01:10:33 +0700

    chore: remove warn by using comment

--- backend/rust-service/src/application/repository/user/report.rs
+++ backend/rust-service/src/application/repository/user/report.rs
@@ -1,11 +1,11 @@
-use sqlx::Transaction;
+// use sqlx::Transaction;
 
-// todo: CRUD report user and guild
-// * Status will stay at pending until moderator doing action
-// * when resolve will get filter (Front-end, I'll make it later)
-// * on change from resloved just clear reslove time
-// * 
-pub async fn report_user_guild(
-    tx: &mut Transaction<'_, sqlx::Postgres>
+// // todo: CRUD report user and guild
+// // * Status will stay at pending until moderator doing action
+// // * when resolve will get filter (Front-end, I'll make it later)
+// // * on change from resloved just clear reslove time
+// // * 
+// pub async fn report_user_guild(
+//     tx: &mut Transaction<'_, sqlx::Postgres>
     
-) {}
+// ) {}
