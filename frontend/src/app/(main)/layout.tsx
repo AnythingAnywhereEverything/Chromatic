9c27dcc8bbc9b97e357b4f7184e9bfdd1b6716a1
Zartex <56326190+ZartexVertagen@users.noreply.github.com>
2026-09-09 11:39:58 +0700

    refactor(layout): move auth layout to main

--- frontend/src/app/(main)/layout.tsx
+++ frontend/src/app/(main)/layout.tsx
@@ -1,13 +1,19 @@
 "use client";
 
-import { useUser } from "@/hooks/useUser";
 import MainLayout from "../_components/layout/main-layout";
 
-export default function AuthShell({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
-
-    return <MainLayout>{children}</MainLayout>;
+export default function AuthShell({ children }: { children: React.ReactNode }) {
+    return (
+        <MainLayout>
+            <main
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    width: "100%",
+                }}
+            >
+                {children}
+            </main>
+        </MainLayout>
+    );
 }
