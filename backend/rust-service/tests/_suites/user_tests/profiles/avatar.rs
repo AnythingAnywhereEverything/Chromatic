13276ace6d7ae73008fe416770a008a3a5621381
seinaru <82324764+K-Hongkaew@users.noreply.github.com>
2026-09-24 04:26:08 +0700

    chore: comment test case

--- backend/rust-service/tests/_suites/user_tests/profiles/avatar.rs
+++ backend/rust-service/tests/_suites/user_tests/profiles/avatar.rs
@@ -1,17 +1,17 @@
-use crate::common::{TestContext, user::create_user};
-use crate::common::media::generate_testimage;
-use axum_test::multipart::{MultipartForm, Part};
-use rs_vips::VipsImage;
-use sqlx::PgPool;
+// use crate::common::{TestContext, user::create_user};
+// use crate::common::media::generate_testimage;
+// use axum_test::multipart::{MultipartForm, Part};
+// use rs_vips::VipsImage;
+// use sqlx::PgPool;
 
-#[sqlx::test]
-async fn test_upload_avatar(pool: PgPool) {
-    let test_id = "test_upload_avatar";
-    let mut ctx = TestContext::new(Some(pool), Some(test_id)).await;
-    let user = create_user(
-        &mut ctx,
-        "testuser",
-        "example@gmail.com",
-        "VeryStrongPassword123!",
-    ).await;
-}
+// #[sqlx::test]
+// async fn test_upload_avatar(pool: PgPool) {
+//     let test_id = "test_upload_avatar";
+//     let mut ctx = TestContext::new(Some(pool), Some(test_id)).await;
+//     let user = create_user(
+//         &mut ctx,
+//         "testuser",
+//         "example@gmail.com",
+//         "VeryStrongPassword123!",
+//     ).await;
+// }
